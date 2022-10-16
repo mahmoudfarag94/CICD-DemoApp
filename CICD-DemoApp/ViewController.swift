85b66259ed9d5fca37afc1df53e47e370554c824
Essam Mahmoud <essam.mahmoud92@hotmail.com>
2022-10-16 14:20:02 +0200

    Initial Commit

---
+++ CICD-DemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICD-DemoApp
+//
+//  Created by Mahmoud Farag on 10/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
